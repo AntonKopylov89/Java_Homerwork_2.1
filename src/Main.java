47069f8fe7580696e77f6ac83e22e1378856aba0
AntonKopylov89 <ttxb@yandex.ru>
2022-04-24 15:47:58 +0400

    Create project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 3700;
+        int bonusMileCost = 20;
+        int bonusMiles = ticketCost / bonusMileCost;
+
+        System.out.println("Количество бонусных миль: " + bonusMiles);
+    }
+}
